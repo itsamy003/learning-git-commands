6aaf24aed3251de0cb084a26cb816a8e8118e063
ammikovvuri <ammi.kovvuri@gmail.com>
2020-03-01 17:45:33 +0530

    added range incompletion functionality

--- array_loop.js
+++ array_loop.js
@@ -5,3 +5,8 @@ var evenArray = arr.filter(v => v%2 === 0);
 
 // add next loop here
 // add next loop here
+
+// filling incomplete values in a range
+let inputArray = [5,6,7,8,10,11,12,16,20];
+let [min, max] = [Math.min(...inputArray), Math.max(...inputArray)];
+Array.from(Array(max-min), (v,i) => i+min).filter(v => !inputArray.includes(v));
